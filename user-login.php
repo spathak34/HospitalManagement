1b8c93dcf0712d1ca81e2a7e9b11b45ee6843a3d
spathak34 <pathaksatyam34@gmail.com>
2020-10-03 18:09:59 +0530

    All New Files Added

--- user-login.php
+++ user-login.php
@@ -1,32 +1,35 @@
 <?php
 session_start();
+error_reporting(0);
 include("include/config.php");
 if(isset($_POST['submit']))
 {
-$ret=mysqli_query($con,"SELECT * FROM doctors WHERE docEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$ret=mysqli_query($con,"SELECT * FROM users WHERE email='".$_POST['username']."' and password='".md5($_POST['password'])."'");
 $num=mysqli_fetch_array($ret);
 if($num>0)
 {
-$extra="dashboard.php";
-$_SESSION['dlogin']=$_POST['username'];
+$extra="dashboard.php";//
+$_SESSION['login']=$_POST['username'];
 $_SESSION['id']=$num['id'];
+$host=$_SERVER['HTTP_HOST'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=1;
-$log=mysqli_query($con,"insert into doctorslog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['dlogin']."','$uip','$status')");
-$host=$_SERVER['HTTP_HOST'];
+// For stroing log if user login successfull
+$log=mysqli_query($con,"insert into userlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['login']."','$uip','$status')");
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
 exit();
 }
 else
 {
-$host  = $_SERVER['HTTP_HOST'];
-$_SESSION['dlogin']=$_POST['username'];
+	// For stroing log if user login unsuccessfull
+$_SESSION['login']=$_POST['username'];	
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=0;
-mysqli_query($con,"insert into doctorslog(username,userip,status) values('".$_SESSION['dlogin']."','$uip','$status')");
+mysqli_query($con,"insert into userlog(username,userip,status) values('".$_SESSION['login']."','$uip','$status')");
 $_SESSION['errmsg']="Invalid username or password";
-$extra="index.php";
+$extra="user-login.php";
+$host  = $_SERVER['HTTP_HOST'];
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
 exit();
@@ -38,7 +41,7 @@ exit();
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Doctor Login</title>
+		<title>User-Login</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
@@ -60,7 +63,7 @@ exit();
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../../index.html">	<h2> HMS | Doctor Login</h2></a>
+				<a href="../index.html"><h2> HMS | Patient Login</h2></a>
 				</div>
 
 				<div class="box-login">
@@ -90,8 +93,12 @@ exit();
 									Login <i class="fa fa-arrow-circle-right"></i>
 								</button>
 							</div>
-							
-						
+							<div class="new-account">
+								Don't have an account yet?
+								<a href="registration.php">
+									Create an account
+								</a>
+							</div>
 						</fieldset>
 					</form>
 
